dd5e4cee4a8f0017cf7a21b557ad76e7e662de1f
danbaragan <daniel.mihai.baragan@gmail.com>
2017-02-08 19:47:11 +0200

    Fixes.

--- deploy/solr/own-scripts/r-config-diff.sh
+++ deploy/solr/own-scripts/r-config-diff.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
-#echo Connecting with user $EDW_MACHINE_USER and host $EDW_MACHINE_HOST
-#echo Running docker-compose on remote from dir $EDW_MACHINE_DIR
+#echo Connecting with user $EDW_DEPLOY_USER and host $EDW_DEPLOY_HOST
+#echo Running docker-compose on remote from dir $EDW_DEPLOY_DIR
 #docker-compose exec solr config-diff.sh
 
-#ssh $EDW_MACHINE_USER@$EDW_MACHINE_HOST << _eof_
-#pwd
-#docker-compose ps
-#_eof_
-
-ssh -q -t $EDW_MACHINE_USER@$EDW_MACHINE_HOST "cd ${EDW_MACHINE_DIR} &&\
-docker-compose exec solr config-diff.sh"
+ssh -q -t $EDW_DEPLOY_USER@$EDW_DEPLOY_HOST "cd ${EDW_DEPLOY_DIR} && \
+env \`xargs < .env\` docker-compose exec solr config-diff.sh"
